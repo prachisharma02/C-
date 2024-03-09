062d3858cde28c3efdbbabd69ac39621d4ace823
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2024-03-09 17:03:18 +0530

    creating only ui

--- ADODemo/Program.cs
+++ ADODemo/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace ADODemo
 {
@@ -10,6 +12,31 @@ namespace ADODemo
     {
         static void Main(string[] args)
         {
+            Program.Connection();
+            Console.ReadLine();
+        }
+
+        static void Connection()
+        {
+            string cs = "Data Source=LAPTOP-ERN1FTED; Initial Catalog=StudDb; Integrated security=true;";
+            SqlConnection conn = new SqlConnection(cs);
+
+            try
+            {
+                conn.Open();
+                if (conn.State != ConnectionState.Open)
+                {
+                    Console.WriteLine("Connection created Successfully");
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();  // Ensure connection is closed even on exceptions
+            }
         }
     }
 }
