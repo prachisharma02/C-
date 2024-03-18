2a47136335de2affc3de968c64e3383fccae5cab
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2024-03-18 12:53:39 +0530

    done calculator with validation

---
+++ VBCalculator/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="WebForm1.aspx.vb" Inherits="VBCalculator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+         <div>
+        <asp:Label runat="server">Num1</asp:Label>
+        <asp:TextBox ID="txtNum1" runat="server" TextMode="Number"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="rfvNum1" runat="server" ControlToValidate="txtNum1" ErrorMessage="Please enter a number for Num1." ForeColor="Red" />
+        <%--<asp:RangeValidator ID="rvAge" runat="server" ControlToValidate="txtNum1" 
+ MinimumValue="120" MaximumValue="18"                    
+            ErrorMessage="num1 must be between 18 and 120." 
+                    ForeColor="orange" />--%>
+        <asp:Label runat="server">Num2</asp:Label>
+        <asp:TextBox ID="txtNum2" runat="server" TextMode="Number"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="rfvNum2" runat="server" ControlToValidate="txtNum2" ErrorMessage="Please enter a number for Num2." ForeColor="Red" />
+
+
+      <br />
+      <br />
+      <asp:TextBox ID="TextBox3" runat="server" TextMode="Number"></asp:TextBox>
+         <br />
+         <br />
+
+      <asp:Button ID="Add" runat="server" onclick="Add_Click" Text="Add" />
+      <asp:Button ID="Subtract" runat="server" onclick="Subtract_Click" Text="Subtract" />     
+      <asp:Button ID="Multiply" runat="server" onclick="Multiply_Click" Text="Multiply" />
+      <asp:Button ID="Divide" runat="server" onclick="Divide_Click" Text="Divide" />
+      <asp:Button ID="Modulus" runat="server" onclick= "Modulus_Click" Text="Modulus" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ VBCalculator/WebForm1.aspx.vb
@@ -0,0 +1,84 @@
+﻿
+
+
+
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+    Protected Sub Add_Click(sender As Object, e As EventArgs) Handles Add.Click
+        Dim num1 As Double = txtNum1.Text
+        Dim num2 As Double = txtNum2.Text
+
+        Dim sum As Integer
+        sum = num1 + num2
+        TextBox3.Text = sum
+        If Double.IsNaN(num1) Or Double.IsNaN(num2) Then
+            ' Handle invalid input (e.g., show an error message)
+            Response.Write("Please enter valid numbers.")
+        Else
+            Dim result As Double = num1 + num2
+            ' Do something with the result (e.g., display it on a label)
+        End If
+    End Sub
+    Protected Sub Subtract_Click(sender As Object, e As EventArgs) Handles Subtract.Click 'id dete hain
+        Dim num1 As Double = txtNum1.Text
+        Dim num2 As Double = txtNum2.Text
+        Dim Subtract As Integer
+        Subtract = num1 - num2
+        TextBox3.Text = Subtract
+        If Double.IsNaN(num1) Or Double.IsNaN(num2) Then
+            ' Handle invalid input (e.g., show an error message)
+            Response.Write("Please enter valid numbers.")
+        Else
+            Dim result As Double = num1 - num2
+            ' Do something with the result (e.g., display it on a label)
+        End If
+    End Sub
+    Protected Sub Multiply_Click(sender As Object, e As EventArgs) Handles Multiply.Click
+        Dim num1 As Double = txtNum1.Text
+        Dim num2 As Double = txtNum2.Text
+        Dim mul As Integer
+        mul = num1 * num2
+        TextBox3.Text = mul
+        If Double.IsNaN(num1) Or Double.IsNaN(num2) Then
+            ' Handle invalid input (e.g., show an error message)
+            Response.Write("Please enter valid numbers.")
+        Else
+            Dim result As Double = num1 * num2
+            ' Do something with the result (e.g., display it on a label)
+        End If
+    End Sub
+    Protected Sub Divide_Click(sender As Object, e As EventArgs) Handles Divide.Click
+        Dim num1 As Double = txtNum1.Text
+        Dim num2 As Double = txtNum2.Text
+        Dim div As Integer
+        div = num1 / num2
+        TextBox3.Text = div
+        If Double.IsNaN(num1) Or Double.IsNaN(num2) Then
+            ' Handle invalid input (e.g., show an error message)
+            Response.Write("Please enter valid numbers.")
+        Else
+            Dim result As Double = num1 / num2
+            ' Do something with the result (e.g., display it on a label)
+        End If
+    End Sub
+    Protected Sub Modulus_Click(sender As Object, e As EventArgs) Handles Modulus.Click
+        Dim num1 As Double = txtNum1.Text
+        Dim num2 As Double = txtNum2.Text
+        Dim modl As Integer
+        modl = num1 Mod num2
+        TextBox3.Text = modl
+        If Double.IsNaN(num1) Or Double.IsNaN(num2) Then
+            ' Handle invalid input (e.g., show an error message)
+            Response.Write("Please enter valid numbers.")
+        Else
+            Dim result As Double = num1 Mod num2
+            ' Do something with the result (e.g., display it on a label)
+        End If
+    End Sub
+
+
+End Class
