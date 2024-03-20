aef446b693af34102a330a055e427478ae12dd4f
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2024-03-20 12:45:40 +0530

    5th fibonacci

--- VBDemo/Class5.vb
+++ VBDemo/Class5.vb
@@ -1,6 +1,20 @@
 ﻿Public Class Class5
     Public Sub Fibonacci()
 
+        Console.WriteLine("enter the size of values")
+        Dim size = Console.ReadLine()
+        Dim i As Integer = 1
+        Dim a As Integer = 1
+        Dim b As Integer = 1
+        Dim c As Integer
+
+        While i <= size
+            Console.WriteLine(a)
+            c = a + b
+            a = b
+            b = c
+            i += 1
+        End While
 
     End Sub
 
